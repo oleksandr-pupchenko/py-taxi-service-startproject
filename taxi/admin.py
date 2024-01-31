1444d743c01edc19830b88853bcd76ea31313a78
Valentyn <sonik2001www@gmail.com>
2024-01-31 11:28:13 +0200

    make reformat with black

--- taxi/admin.py
+++ taxi/admin.py
@@ -8,9 +8,13 @@ admin.site.register(Manufacturer)
 
 @admin.register(Driver)
 class AdminDrive(UserAdmin):
-    list_display = UserAdmin.list_display + ("license_number", )
-    fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("license_number", )}), )
-    add_fieldsets = UserAdmin.add_fieldsets + (("Additional info", {"fields": ("license_number", )}), )
+    list_display = UserAdmin.list_display + ("license_number",)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("license_number",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("license_number",)}),
+    )
 
 
 @admin.register(Car)
